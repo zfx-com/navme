7048a3a95df3d8da73ebdd0081b9bcc9df142d07
Kravchenko Igor <gornivv@gmail.com>
2021-05-29 22:26:57 +0300

    fix: reload navigation in  debug build

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -5,6 +5,7 @@ import 'navigation/index.dart';
 
 class App extends StatefulWidget {
   App({Key key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _AppState();
 }
@@ -14,11 +15,17 @@ class _AppState extends State<App> {
   final StateRouteInformationParser _stateRouteInformation =
       StateRouteInformationParser();
 
+  PlatformRouteInformationProvider _routeInformationProvider;
+  PlatformRouteInformationProvider get routeInformationProvider =>
+      _routeInformationProvider ??= PlatformRouteInformationProvider(
+          initialRouteInformation: const RouteInformation(location: '/'));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerDelegate: _routerDelegate,
       routeInformationParser: _stateRouteInformation,
+      routeInformationProvider: routeInformationProvider,
     );
   }
 }
